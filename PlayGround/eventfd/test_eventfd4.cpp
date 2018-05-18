626eb3468a63f7474e0628d8c4a38500e88eb801
SamsonWang <zl.wang@foxmail.com>
2018-05-18 19:58:56 +0800

    eventfd test code

--- PlayGround/eventfd/test_eventfd4.cpp
+++ PlayGround/eventfd/test_eventfd4.cpp
@@ -14,17 +14,21 @@ int main(int argc, char *argv[])
 {
     UNUSED(argc);
     UNUSED(argv);
+
+    // nonblock eventfd
+    int efd1 = eventfd(0, 0);
+    int efd2 = eventfd(0, 0);
     
-    int efd = eventfd(0, 0);
-    if (efd == -1)
+    if (efd1 == -1 || efd2 == -1)
     {
         perror("eventfd failed");
         return -1;
     }
-    printf("master thread: efd=%d\n", efd);
+    
+    printf("master thread: efd1=%d efd2=%d\n", efd1, efd2);
 
     pthread_t tid;
-    int ret = pthread_create(&tid, NULL, func_child, (void*)&efd);
+    int ret = pthread_create(&tid, NULL, func_child, (void*)&efd1);
     if (ret != 0)
     {
         perror("pthread_create failed");
@@ -36,7 +40,7 @@ int main(int argc, char *argv[])
     {
         ++val;
         
-        ssize_t ret = write(efd, (const void*)&val, sizeof(val));
+        ssize_t ret = write(efd1, (const void*)&val, sizeof(val));
         if (ret == -1)
         {
             perror("master thread write failed");
@@ -46,7 +50,7 @@ int main(int argc, char *argv[])
             printf("master thread write(%d): %lld\n", ret, val);
         }
 
-        // write to efd every 1 second
+        // write to efd1 every 1 second
         sleep(1);
     }
     
@@ -61,8 +65,8 @@ int main(int argc, char *argv[])
 
 void* func_child(void* args)
 {
-    int efd = *((int*)(args));
-    printf("child thread: efd=%d\n", efd);
+    int efd1 = *((int*)(args));
+    printf("child thread: efd1=%d\n", efd1);
 
     int64_t val = 0;
     int nCount = 10;
@@ -76,18 +80,18 @@ void* func_child(void* args)
     
     do {
         FD_ZERO(&objFdSet);
-        FD_SET(efd, &objFdSet);
+        FD_SET(efd1, &objFdSet);
         
-        int ret = select(efd+1, &objFdSet, NULL, NULL, NULL);
+        int ret = select(efd1+1, &objFdSet, NULL, NULL, NULL);
         if (ret < 0)
         {
             perror("select failed");
         }
         else
         {
-            printf("child thread select: %d\n", ret);
+            // printf("child thread select: %d\n", ret);
             
-            ret = read(efd, (void*)&val, sizeof(val));
+            ret = read(efd1, (void*)&val, sizeof(val));
             if (ret == -1)
             {
                 perror("read failed:");
