a7cce1f847f540fd01ce26017d731f66eb9daa2a
SamsonWang <zl.wang@foxmail.com>
2018-08-13 10:42:58 +0800

    update leetcode 0005

--- LeetCode/0005/0005_3.cpp
+++ LeetCode/0005/0005_3.cpp
@@ -14,22 +14,28 @@ string longestPalindrome(string s) {
     for (size_t i=0; i < s.length(); ++i) {
 
         // 如何确定初始的边界很重要
-        
-        
-        for (size_t j=s.length()-i; j != 0; --j) {
+        size_t j = i;
+        size_t k = i;
 
-            if (ans.length() >= j) {
-                continue;
-            }
-            
-            string temp = s.substr(i, j);
-            if (ans.length() < temp.length()
-                && isPalindrome(temp)) {
-                ans = temp;
-            }
+        // 向两边拓展边界
+        while (j-1>=0 && s[j-1]==s[i]) {
+            --j;
         }
 
-        
+        while (k+1<s.length() && s[k+1]==s[i]) {
+            ++k;
+        }
+
+        while (j-1>=0
+               && k+1<s.length()
+               && s[j-1]==s[k+1]) {
+            --j;
+            ++k;
+        }
+
+        if (k-j+1 > ans.length()) {
+            ans = s.substr(j, k-j+1);
+        }
     }
 
     return ans;
@@ -40,15 +46,18 @@ int main(int argc, char* argv[]) {
     string str1 = "abab";
     string str2 = "bb";
     string str3 = "a";
+    string str4 = "babad";
 
     
     string ret1 = longestPalindrome(str1);
     string ret2 = longestPalindrome(str2);
     string ret3 = longestPalindrome(str3);
+    string ret4 = longestPalindrome(str4);
 
     printf("%s---%s\n", str1.c_str(), ret1.c_str());
     printf("%s---%s\n", str2.c_str(), ret2.c_str());
     printf("%s---%s\n", str3.c_str(), ret3.c_str());
+    printf("%s---%s\n", str4.c_str(), ret4.c_str());
     
 
     return 0;
