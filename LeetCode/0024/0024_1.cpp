23a608832e7f41d583e953b46d21d0b1f60bbd6c
SamsonWang <wangzhilv@gmail.com>
2018-10-06 23:22:50 +0800

    leetcode 24

--- LeetCode/0024/0024_1.cpp
+++ LeetCode/0024/0024_1.cpp
@@ -1,7 +1,5 @@
 
-
 #include <iostream>
-
 
 // Definition for singly-linked list.
 struct ListNode {
@@ -13,7 +11,24 @@ struct ListNode {
 
 // 需要考虑到链表的尾部不足两个的情况
 ListNode* swapPairs(ListNode* head) {
+    ListNode preHead(0);
+    preHead.next = head;
 
+    ListNode* p0 = &preHead;
+    ListNode* p1 = p0->next;
+    ListNode* p2 = p1 ? p1->next : NULL;
+
+    while (p1 && p2) {
+        p0->next = p2;
+        p1->next = p2->next;
+        p2->next = p1;
+
+        p0 = p1;
+        p1 = p0->next;
+        p2 = p1 ? p1->next : NULL;
+    }
+
+    return preHead.next;
 }
 
 void printList(ListNode* head, const std::string info) {
@@ -26,15 +41,18 @@ void printList(ListNode* head, const std::string info) {
 }
 
 int main(int argc, char* argv[]) {
-
     ListNode preHead(0);
     ListNode* roll= &preHead;
-    for (int i=1; i<5; ++i) {
+    for (int i=1; i<8; ++i) {
         roll->next = new ListNode(i);
         roll = roll->next;
     }
 
     printList(preHead.next, "before");
+
+    ListNode* ret = swapPairs(preHead.next);
+
+    printList(ret, "after");
 
     return 0;
 }
