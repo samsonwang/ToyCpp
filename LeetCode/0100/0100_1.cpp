a1758f21c91991f60e69e96d180627d7b4f784eb
SamsonWang <wangzhilv@gmail.com>
2018-11-27 16:23:09 +0800

    leetcode 100

---
+++ LeetCode/0100/0100_1.cpp
@@ -0,0 +1,24 @@
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (p==nullptr && q==nullptr) {
+        return true;
+    }
+
+    else if (p != nullptr
+             && q != nullptr
+             && p->val == q->val
+             && isSameTree(p->left, q->left)
+             && isSameTree(p->right, q->right)) {
+        return true;
+    }
+
+    return false;
+}
