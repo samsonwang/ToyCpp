7f78be2142bdd5aaabea9f21ae275b786d9f05fe
SamsonWang <wangzhilv@gmail.com>
2019-01-03 14:45:51 +0800

    leetcode 50

---
+++ LeetCode/0050/0050_3.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+double myPow(double x, int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    double half = myPow(x, n/2);
+    int left = n % 2;
+    if (left == 0) {
+        return half * half;
+    }
+    else if (left > 0) {
+        return half * half * x;
+    }
+    else {
+        return half * half / x;
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "10 % 3," << 10 % 3 << std::endl;
+    std::cout << "-10 % 3," << -10 % 3 << std::endl;
+    std::cout << "10 % -3," << 10 % -3 << std::endl;
+    std::cout << "-10 % -3," << -10 % -3 << std::endl;
+
+    std::cout << myPow(3, 0) << std::endl;
+
+    return 0;
+}
