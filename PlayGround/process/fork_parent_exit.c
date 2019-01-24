e824ddbbb3addb874051da79611f62a8dc59fcd8
SamsonWang <wangzhilv@gmail.com>
2019-01-24 15:41:30 +0800

    test for child and parent process

--- PlayGround/process/fork_parent_exit.c
+++ PlayGround/process/fork_parent_exit.c
@@ -2,40 +2,57 @@
 /*
   在linux下，父进程被杀死而子进程仍然运行，则该子进程就变成了孤儿进程。
   如何终止父进程及其子进程
-
 */
 
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/prctl.h>
 
 void parent() {
+    int count = 0;
     while(true) {
+        printf("parent: running\n");
         sleep(5);
-        printf("parent is running\n");
+        ++count;
+        if (count > 3) {
+            printf("parent: call exit\n");
+            exit(0);
+        }
     }
 }
 
 void child() {
+    prctl(PR_SET_PDEATHSIG, SIGHUP);
     while(true) {
+        printf("child : running\n");
         sleep(5);
-        printf("child is running\n");
     }
 }
 
+void proc_sighup(int sig) {
+    printf("%05d :sighup received\n", getpid());
+    exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char* argv[]) {
-    int pid = fork();
-    if (pid) {
+
+    signal(SIGHUP, proc_sighup);
+
+    switch (fork()) {
     case -1:
         perror("fail to fork");
         break;
     case 0:
-        printf("parent process");
-        parent();
+        // printf("child process");
+        printf("child : pid = %d, ppid = %d\n",
+               getpid(), getppid());
+        child();
         break;
     default:
-        printf("child process");
-        child();
+        printf("parent: pid = %d\n", getpid());
+        parent();
         break;
     }
 
